92a63903dec18cc04673de91ce50ad752973f0d0
prazgaitis <prazgaitis@gmail.com>
2017-12-12 22:09:31 -0600

    remove monkeypatch

--- src/day4.2.rb
+++ src/day4.2.rb
@@ -1,17 +1,11 @@
 input = File.readlines("data/day4.txt")
 valid = 0
 
-class String
-  def anagram? word
-    self.chars.sort.join == word.chars.sort.join
-  end
-end
-
 input.each do |line|
 	words = []
 	duplicates = false
 	line.split(" ").each do |word|
-		if words.any? {|x| x.anagram?(word) }
+    if words.any? {|x| x.chars.sort.join == word.chars.sort.join }
 			duplicates = true
 		end
 		words << word
